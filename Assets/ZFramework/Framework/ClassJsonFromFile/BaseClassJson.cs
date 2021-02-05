dffc12538b8c9fa02547cff71d8a4470fb46068e
黄晨阳\19063 <1906375838@qq.com>
2021-02-05 22:05:09 +0800

    添加自读取配置模块

---
+++ Assets/ZFramework/Framework/ClassJsonFromFile/BaseClassJson.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using ZFramework.ClassExt;
+
+namespace ZFramework.ClassJsonFromFile
+{
+    /// <summary>
+    /// 底层类
+    /// </summary>
+    public partial class BaseClassJson<T> : IClassJson<T> where T : class, new()
+    {
+        /// <summary>
+        /// 存放Json文件的文件夹路径
+        /// </summary>
+        public readonly static string ClassJsonFileDir = null;
+
+        /// <summary>
+        /// 存放json文件的文件路径
+        /// </summary>
+        public readonly static string ClassJsonFilePath = null;
+
+        /// <summary>
+        /// 每个继承类型中的唯一从json文件中读取的实例
+        /// </summary>
+        private static T instanceT = null;
+
+        static BaseClassJson()
+        {
+            ClassJsonFileDir = Path.Combine(Application.persistentDataPath, "BaseClassJson");
+            ClassJsonFilePath = Path.Combine(ClassJsonFileDir, string.Format("{0}.json", typeof(BaseClassJson<T>).Name));
+            if (File.Exists(ClassJsonFilePath))
+            {
+                string jsonContent = ClassJsonFilePath.GetTextAssetContentStr();
+                if (string.IsNullOrEmpty(jsonContent))
+                {
+                    instanceT = new T();
+                }
+                else
+                {
+                    instanceT = jsonContent.ToNewtonObjectT<T>();
+                }
+            }
+            else
+            {
+                instanceT = new T();
+            }
+        }
+
+        public T GetT
+        {
+            get
+            {
+                return instanceT;
+            }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                return typeof(BaseClassJson<T>).Name;
+            }
+        }
+
+        public string FileNameWithExtension
+        {
+            get { return string.Format("{0}.json", FileName); }
+        }
+
+        public bool HasFile()
+        {
+            return File.Exists(ClassJsonFilePath);
+        }
+
+        public void SaveTIfExist()
+        {
+            string jsonContent = instanceT.ToNewtonJson();
+            ClassJsonFilePath.WriteTextAssetContentStr(jsonContent);
+        }
+    }
+}
---
+++ Assets/ZFramework/Framework/ClassJsonFromFile/IClassJson.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZFramework.ClassJsonFromFile
+{
+    /// <summary>
+    /// 接口
+    /// </summary>
+    public interface IClassJson<T> where T:class,new()
+    {
+        /// <summary>
+        /// 直接从json文件中获取指定类型实例
+        /// </summary>
+        /// <returns></returns>
+        T GetT { get; }
+
+        /// <summary>
+        /// 文件的名字
+        /// </summary>
+        string FileName
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 文件的名字带后缀
+        /// </summary>
+        string FileNameWithExtension
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 是否存在此实例类型的json文件
+        /// </summary>
+        /// <returns></returns>
+        bool HasFile();
+
+        /// <summary>
+        /// 创建实例文件并存储，有的话就覆盖
+        /// </summary>
+        void SaveTIfExist();
+    }
+}
